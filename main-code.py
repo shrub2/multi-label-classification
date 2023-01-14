51e6a41a17b25ddeaf3a9d12a549f2808c1287ca
titananalyst <75612709+titananalyst@users.noreply.github.com>
2023-01-14 13:30:34 +0100

    Update main-code.py
    Added test_accuracy, implemented it in epoch and added graph_accuracy.
    Both are executed below if __name__ == '__main__':
    with graph_accuracy(epoch(num_epochs, train_loader))

--- main-code.py
+++ main-code.py
@@ -11,13 +11,14 @@ import torchvision.transforms as transforms
 
 from PIL import Image
 
-# import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 import torch.optim as optim
 abspath = os.path.abspath(__file__)
 print(abspath)
+
+print(f'Do I have GPU? : {torch.cuda.is_available()}')  # if True you have gpu which can be used, otherwise not!
 
 data_path = "images/train/"
 
@@ -30,12 +31,12 @@ classes = np.unique(products['GS1 Form'])
 batch_size = 64
 num_classes = len(classes)
 learning_rate = 0.001
-num_epochs = 10
+num_epochs = 5
 
 # change this variable to save and load specific models
 # to change the classifier optimizer you still need to change
 # it in the code 
-classifier_saved = 'models/model_ai_Adamax_2.pth'
+classifier_saved = 'models/model_ai_Adamax_3.pth'
 
 # normalizing data ...
 transform = transforms.Compose([transforms.Resize((32,32)),
@@ -46,6 +47,8 @@ transform = transforms.Compose([transforms.Resize((32,32)),
 
 # Device will determine whether to run the training on GPU or CPU.
 # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# print(device)  # check used device "cuda:0" for gpu or "cpu"
+# print(torch.cuda.current_device())  # prints 0 for gpu, -1 for cpu
 
 """Loading data with PyTorch"""
 
@@ -90,7 +93,15 @@ class CNN(nn.Module):
 """AI generated optimizer"""
 
 cnn = CNN() # define your CNN
+
+# cnn.to(device) # move the model to GPU
+# images = images.to(device)
+# labels = labels.to(device)
+
 optimizer = optim.Adamax(cnn.parameters(), lr=learning_rate) # define the optimizer
+# optimizer.to(device)
+
+# print(cnn.conv1.weight.device)  # prints "cuda:0" for gpu or "cpu"
 
 # print("test")
 """Neural network training"""
@@ -98,7 +109,9 @@ optimizer = optim.Adamax(cnn.parameters(), lr=learning_rate) # define the optimi
 def epoch(num_epochs, train_loader):
     # We use the pre-defined number of epochs to determine how many iterations to train the network on
     total_time = 0
+    accuracies = []
     for epoch in range(num_epochs):
+        print(f'Epoch: {epoch + 1}')
         epoch_time_start = time.time()
         #Load in the data in batches using the train_loader object
         for i, (images, labels) in enumerate(train_loader): 
@@ -109,8 +122,8 @@ def epoch(num_epochs, train_loader):
             # images = images.to(device)
             # labels = labels.to(device)
             
-            images = images.to()
-            labels = labels.to()
+            # images = images.to()
+            # labels = labels.to()
             
             # forward pass
             output = cnn(images) # pass the input data through the network
@@ -123,11 +136,21 @@ def epoch(num_epochs, train_loader):
         epoch_time_end = time.time()
         epoch_time = round(epoch_time_end - epoch_time_start, 2)
         total_time += epoch_time
-
+        
         print('Epoch time: ', epoch_time)
         print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
-    
+
+        # measure accuracy per epoch
+        # num_iterations = num_epochs
+        # for epoch in range(num_iterations):
+            # epoch(1, train_loader)
+        print(f'Measruring Epoch {epoch + 1}')
+        accuracy = test_accuracy(cnn, test_loader)
+        accuracies.append(accuracy)
+        print(f'Stored accuracies: {accuracies}')
+
     print('Total time: ', round(total_time, 2))
+    return accuracies
         
 
 def accuracy(cnn_):
@@ -138,6 +161,41 @@ def accuracy(cnn_):
 
 
     # print("test")
+
+# test loader
+test_data = torchvision.datasets.ImageFolder(root=data_path, 
+                                            transform=transform)
+test_loader = torch.utils.data.DataLoader(dataset = test_data,
+                                        batch_size = batch_size,
+                                        pin_memory = True,
+                                        shuffle = True)
+
+
+def test_accuracy(model, test_loader):
+    model.eval() # set model to evaluation mode
+    correct = 0
+    total = 0
+    print(f'Testing accuracy cumulative epochs\n please wait...')
+    with torch.no_grad(): # temporarily set all the requires_grad flag to false
+        for images, labels in test_loader:
+            # print(f'Doing something: {labels}')
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    accuracy_raw = correct / total
+    accuracy = 100 * accuracy_raw
+    print('Accuracy of the network on the test images: %d %%' % (accuracy))
+    return accuracy_raw
+
+# graphs
+def graph_accuracy(accuracy):
+    plt.plot(accuracy)
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.show()
+
+
 
 def saver(cnn_):
     # saving model
@@ -153,8 +211,6 @@ def test():
         # loading model
         cnn = CNN()
         cnn.load_state_dict(torch.load(classifier_saved))
-
-
 
         # prediction testing
 
@@ -174,12 +230,13 @@ def test():
         img = mpimg.imread(img_path)
         imgplot = plt.imshow(img)
         plt.axis('off')
-        # plt.show()
+        plt.show()
     # accuracy(cnn)
 
 if __name__ == '__main__':
-    epoch(num_epochs, train_loader)
-
+    # epoch(num_epochs, train_loader)
+    graph_accuracy(epoch(num_epochs, train_loader))
+    # test_accuracy(cnn, test_loader)
     saver(cnn)
 
     #img = 'images/test/test_4.jpg'
